d789cc65974806f624e81df45ea0b3f21900eacb
rennavittorio <renna.vittorio@gmail.com>
2023-05-04 15:14:10 +0200

    add: add ProjectShow component to show project details from api with slug

--- src/pages/ProjectShow.vue
+++ src/pages/ProjectShow.vue
@@ -2,9 +2,24 @@
 
     <Default>
 
-        <h1 class="text-center">
-            my detailed card
-        </h1>
+        <div class="container">
+            
+            <template v-if="loading === false">
+
+                <Card
+                :title="project.title"
+                :project_slug="project.slug"
+                :project_category="project.proj_category"
+                :technologies="project.technologies"
+                />
+
+            </template>
+
+            <template v-else>
+                now loading...
+            </template>
+
+        </div>
 
     </Default>
 
@@ -12,10 +27,38 @@
 </template>
 <script>
 import Default from '../layouts/Default.vue';
+import Card from '../components/Card.vue';
+import axios from 'axios';
 
 export default {
     components: {
         Default,
+        Card,
+    },
+    props: ['slug'],
+    data(){
+        return {
+            project: null,
+            loading: true,
+        }
+    }, 
+    methods: {
+        fetchProject(slug){
+
+            this.loading = true
+
+            axios.get(`http://127.0.0.1:8000/api/projects/${slug}`).then(res=>{
+                const { success, project } = res.data;
+                this.project = project;
+                console.log(this.project)
+            }).finally(()=>{
+                this.loading = false
+                console.log('show me')
+            })
+        }
+    },
+    created(){
+        this.fetchProject(this.slug);
     }
     
 }
